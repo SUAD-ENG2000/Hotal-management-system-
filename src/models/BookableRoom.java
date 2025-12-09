a6f6fa44bbb27feb5c6eef1881070ede8a90bd06
SUAD-ENG2000 <souad20184013@gmail.com>
2025-12-09 05:06:12 +0200

    update BookableRoom class

--- src/models/BookableRoom.java
+++ src/models/BookableRoom.java
@@ -1,5 +1,26 @@
+// BookableRoom.java
 package models;
 
-public class BookableRoom {
+/**
+ * A room that can be booked, implementing the IBookable interface.
+ */
+public class BookableRoom extends Room implements IBookable {
+    public BookableRoom(String roomNumber, String roomType, double pricePerNight, boolean isAvailable) {
+        super(roomNumber, roomType, pricePerNight, isAvailable);
+    }
 
+    @Override
+    public boolean checkAvailability() {
+        return isAvailable();
+    }
+
+    @Override
+    public void markAsBooked() {
+        setAvailable(false);
+    }
+
+    @Override
+    public void markAsAvailable() {
+        setAvailable(true);
+    }
 }
