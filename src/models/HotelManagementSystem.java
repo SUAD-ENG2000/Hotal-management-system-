af051ce5293412b950c871ce0397175e24c15de7
SUAD-ENG2000 <souad20184013@gmail.com>
2025-12-17 23:08:07 +0200

    update HotelManagementSystem class

--- src/models/HotelManagementSystem.java
+++ src/models/HotelManagementSystem.java
@@ -1,13 +1,20 @@
-
 package models;
+import views.LoginWindow;
+import javax.swing.*;
 
 public class HotelManagementSystem {
     public static void main(String[] args) {
-       
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new views.LoginWindow().setVisible(true);
-            }
+        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        SwingUtilities.invokeLater(() -> {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.setVisible(true);
         });
     }
 }
